8ea9e37a669efcd8756301e06f8f13e7aa176a59
Sgayatri0519 <saimosuru0510@gmail.com>
2026-05-13 15:42:51 +0530

    Add files via upload
    what is python?
    what are the tokens?
    what are the identifiers?
    what are the variables?
    what is boolean?
    what is errors and types of error

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("hello world")
+"create a variables and datatypes"
+"students details"
+name = "pandu"
+age = 25
+price = 99.99
+is_student = True
+age = 25
+year = 2026
+
+print(type(age))
+print(type(price))
+print(type(year))
+a=
