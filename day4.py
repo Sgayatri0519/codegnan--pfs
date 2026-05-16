7b73482615c189b02507d756db7788c327510b23
Mosuru sai <saimosuru0510@gmail.com>
2026-05-16 12:30:18 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,44 @@
+#concentation:the (+)for int and can ,but for the other data types it will act as concentation the data types
+"""
+a=90
+b=8
+print(a+b)
+any_="python"
+so="is a languages"
+print(any_+so)
+an=[1,2]
+am=[3,4]
+print(an+am)
+
+#tuple:collection of different data types separate by commas, represented in () and immuable
+
+
+some=(1,"python",[1,2],[3,4])
+print(some.count("python"))
+
+
+#method:count():this is used to count the particular item in the tuple
+#syntax:variable_name .count(item)
+
+some=(1,"python",[1,2],[3,4])
+print(some.index("python"))
+some=(1,[1,2],[3,4],"python")
+any =[1,"python",[1,2,[34,"this is python 2nd class",78],"python is language",1,2],34,[3,4]]
+print(any[2])
+
+
+#dictionary :dict is key :value pair separate by coloum:any pair of separate by commas
+#represented by {}
+
+sai_details={"name":"sai",1:2,(1,2):[3,4]}
+print(type(sai_details))
+
+"""
+
+sai={"name":"sai","age":21,"moblie number":8919221683,"pan":"gpxgnhyhyh"}
+print(type(sai))
+print(sai.keys())
+print(sai.update({"aardhar":123456789021}))
+sai['name']="keerthi"
+print(sai)
+      
