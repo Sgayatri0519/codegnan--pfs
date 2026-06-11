a13d57a166c5bf3e4b393d068402c949d2c780c8
Mosuru sai <saimosuru0510@gmail.com>
2026-06-11 15:26:50 +0530

    Add files via upload

---
+++ day21.py
@@ -0,0 +1,38 @@
+#file handling:is an object of file to maintain serveral function of file like,creating,reading,updateing and deleteing the file.
+#open a file:1.open()2.with open()
+#there are modes:'r'is used to reading the file, error if file does not exit
+#'a' is used to add the txt into file,if file does not exist
+#'w' is used to add the txtx into file but it will override of all txt inside file.if the file does not exist it will create with that name
+#'x' is used create the file but will through error if we are used 'r' mode to create
+#example:
+so=open('demo.txt',"r")
+print(so.read())
+so.close()
+
+#program:
+so=open('dm.txt','w')
+print(so.write('nxt will be java'))
+so.close()
+#program:
+with open('demo.txt','w') as so:
+    print(so.write('java'))
+#methods:write()read(): this method can read entrire file chunk by chunk where we can specify the side
+#readline():can read only one line at a time in a file
+#readlines():it will read entrie file and gives in a list where each line is each index in the list
+so = open( 'demo.txt','r')
+any_.write('hello')
+with open('demo.txt','w'):
+any_.write("hello")
+#program
+any_=open('demo.txt','r')
+print(any_.read())
+any_.close()
+#program
+any_=open('demo.txt','r')
+print(any_readline())
+any_.close()
+#how to remove file
+import os
+
+os.remove("demo.txt")
+print("File deleted successfully")    
